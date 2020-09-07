aefc4aafd01aa4c17c9ebb882f3e55d6293fde6f
Abhishek Kumar Misra <akumarmisra@dxc.com>
2020-09-07 10:49:07 +0530

    TestAddNewPromotionRule added for success

--- PromotionEngine/PromotionEngine.UnitTest/UnitTest1.cs
+++ PromotionEngine/PromotionEngine.UnitTest/UnitTest1.cs
@@ -11,7 +11,22 @@ namespace PromotionEngine.UnitTest
         [TestMethod]
         public void TestAddNewPromotionRule()
         {
-            
+
+            PromotionEngine promotionEngine = new PromotionEngine();
+            CartManager promotedCartManager = new CartManager(new PromotionEngine());
+            Product product1 = new Product() { Id = 1, Name = "A", Price = 50 };
+            Product product2 = new Product() { Id = 2, Name = "B", Price = 30 };
+            Product product3 = new Product() { Id = 3, Name = "C", Price = 20 };
+            Product product4 = new Product() { Id = 4, Name = "D", Price = 15 };
+
+            Cart cart = new Cart() { Id = 1 };
+            cart.AddItems(new CartItem() { Product = product1, Quantity = 1 });
+            cart.AddItems(new CartItem() { Product = product2, Quantity = 1 });
+            cart.AddItems(new CartItem() { Product = product3, Quantity = 1 });
+            int expected = 100;
+            int actual;
+            actual = promotionEngine.ApplyPromotion(cart);
+            Assert.AreEqual(expected, actual);
         }
     
         [TestMethod]
