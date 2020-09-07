3ce2635fba03d2915526e784c22d070a23e68b35
Abhishek Kumar Misra <akumarmisra@dxc.com>
2020-09-07 10:47:29 +0530

    Unit test added skeleton

---
+++ PromotionEngine/PromotionEngine.UnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Business;
+
+namespace PromotionEngine.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddNewPromotionRule()
+        {
+            
+        }
+    
+        [TestMethod]
+        public void TestCheckoutCart()
+        {
+            
+        }
+    }
+}
